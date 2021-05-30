99f8ca7b5a221bac824299002db2b2c84caebec0
3Snake3 <ql422366@gmail.com>
2021-05-30 20:54:47 +0800

    Units deletion

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,4 @@
-new Item("ice-cube");
-new Item("cryocube");
-new Item("creostone");
-new Liquid("creotite");
-new Liquid("ledonite");
-
 require("blocks/blocks");
 require("blocks/turrets");
-require("units/units");
 require("statuses/statuses");
 require("blib");
