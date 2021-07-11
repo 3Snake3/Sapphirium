6a45d50bf53a9bb4c2a3cde01035f0598ded3382
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-07-11 11:46:05 +0300

    Update planets.js

--- scripts/planets.js
+++ scripts/planets.js
@@ -6,24 +6,24 @@ isDev = true;
 
 ///CONTENT///
 
-const Planet = extend(Planet, "test", Planets.sun, 3, 1, {});
+const testPlanet = extend(Planet, "test", Planets.sun, 3, 1, {});
 
-Planet.generator = testGenerator;
-Planet.startSector = 78;
+//tesPlanet.generator = testGenerator;
+testPlanet.startSector = 78;
 
 //Planet.hasAtmosphere = true;
-Planet.atmosphereRadIn = 0.019;
-Planet.atmosphereRadOut = 0.29;
-Planet.atmosphereColor = Color.valueOf("7B5959FF");
+testPlanet.atmosphereRadIn = 0.019;
+testPlanet.atmosphereRadOut = 0.29;
+testPlanet.atmosphereColor = Color.valueOf("7B5959FF");
 	
-Planet.meshLoader = () => new HexMesh(Planet, 6);
+testPlanet.meshLoader = () => new HexMesh(Planet, 6);
 	
-Planet.orbitRadius = 20.4;
-Planet.rotateTime = 10200;
-Planet.orbitTime = Mathf.pow((1.0 + 10.0 + 0.76), 1.5) * 80;
+testPlanet.orbitRadius = 20.4;
+testPlanet.rotateTime = 10200;
+testPlanet.orbitTime = Mathf.pow((1.0 + 10.0 + 0.76), 1.5) * 80;
 
 if(isDev){
-Planet.accessible = true
+testPlanet.accessible = true
 }else{
-Planet.accessible = false
+testPlanet.accessible = false
 }		
