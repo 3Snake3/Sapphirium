33886867f77d57da7cfe6fbf90addb9ff92662fe
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-08-27 17:13:59 +0300

    Code cleanup

--- scripts/statuses/statuses.js
+++ scripts/statuses/statuses.js
@@ -14,52 +14,75 @@ const weakened = extendContent(StatusEffect, "weakened", {
 exports.weakened = weakened; 
 
 const superMelting = extendContent(StatusEffect, "super-melting", {
-init(){
-this.opposite(StatusEffects.wet, StatusEffects.freezing, superFreezing);
-this.affinity(StatusEffects.tarred, ((unit, time, newTime, result) => {
+            
+    init(){
+                
+        this.opposite(StatusEffects.wet, StatusEffects.freezing, superFreezing)
+                
+                
+        this.affinity(StatusEffects.tarred, ((unit, time, newTime, result) => {
+                    
             unit.damagePierce(this.transitionDamage);
             Fx.burning.at(unit.x + Mathf.range(unit.bounds() / 2), unit.y + Mathf.range(unit.bounds() / 2));
+                    
             result.set(superMelting, time);
-        }));
-this.affinity(StatusEffects.melting, ((unit, time, newTime, result) => {
+        }))
+                
+                
+        this.affinity(StatusEffects.melting, ((unit, time, newTime, result) => {
+                    
             unit.damagePierce(this.transitionDamage);
             Fx.melting.at(unit.x + Mathf.range(unit.bounds() / 2), unit.y + Mathf.range(unit.bounds() / 2));
+                    
             result.set(superMelting, time);
-        }));
-this.affinity(StatusEffects.burning, ((unit, time, newTime, result) => {
+        }))
+                
+                
+        this.affinity(StatusEffects.burning, ((unit, time, newTime, result) => {
+                    
             unit.damagePierce(this.transitionDamage);
             Fx.burning.at(unit.x + Mathf.range(unit.bounds() / 2), unit.y + Mathf.range(unit.bounds() / 2));
+                    
             result.set(superMelting, time);
-        }));
-       },
-       });
-        superMelting.healthMultiplier = 0.5;
-        superMelting.speedMultiplier = 0.5;
-        superMelting.reloadMultiplier = 0.5;
-        superMelting.transitionDamage = 45;
-        superMelting.damage = 0.10;
-        superMelting.effect = Fx.melting;
-        superMelting.color = Color.valueOf("dd6f58");
-        exports.superMelting = superMelting;
+        }))
+    },
+            
+    healthMultiplier: 0.5,
+    speedMultiplier: 0.5,
+    reloadMultiplier: 0.5,
+    transitionDamage: 45,
+    damage: 0.1,
+    effect: Fx.melting,
+    color: Color.valueOf("dd6f58")       
+});
         
-        const superFreezing = extendContent(StatusEffect, "super-freezing", {
-init(){
-this.opposite(StatusEffects.burning, StatusEffects.melting, superMelting);
-this.affinity(StatusEffects.blasted, (unit, time, newTime, result) => {
+const superFreezing = extendContent(StatusEffect, "super-freezing", {
+            
+    init(){
+                
+        this.opposite(StatusEffects.burning, StatusEffects.melting, superMelting)
+                
+        this.affinity(StatusEffects.blasted, (unit, time, newTime, result) => {        
             unit.damagePierce(this.transitionDamage);
             result.set(superFreezing, time);
-        });
-this.affinity(StatusEffects.freezing, ((unit, time, newTime, result) => {
+        })
+                
+        this.affinity(StatusEffects.freezing, ((unit, time, newTime, result) => {
+                    
             unit.damagePierce(this.transitionDamage);
             Fx.freezing.at(unit.x + Mathf.range(unit.bounds() / 2), unit.y + Mathf.range(unit.bounds() / 2));
+                    
             result.set(superFreezing, time);
-        }));
-        },
-        });
-        superFreezing.speedMultiplier = 0.5;
-        superFreezing.reloadMultiplier = 0.5;
-        superFreezing.transitionDamage = 10;
-        superFreezing.damage = 0.5;
-        superFreezing.effect = Fx.freezing;
-        superFreezing.color = Color.valueOf("6fdded");
-        exports.superFreezing = superFreezing;
+        }))
+    },
+      
+    speedMultiplier: 0.5,
+    reloadMultiplier: 0.5,
+    transitionDamage: 10,
+    damage: 0.5,
+    effect: Fx.freezing,
+    color: Color.valueOf("6fdded")
+});
+
+exports.superFreezing = superFreezing;
+exports.superMelting = superMelting;
