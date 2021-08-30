ce4f14b593c8c0d5c849f3186e3cca611bedc4ec
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-08-30 11:45:11 +0300

    Update techtree.js

--- scripts/techtree.js
+++ scripts/techtree.js
@@ -3,6 +3,7 @@ const turrets = require("blocks/turrets");
 
 const creostone = Vars.content.getByName(ContentType.item, "adc-creostone"); 
 const globium = Vars.content.getByName(ContentType.item, "adc-globium"); 
+const creotite = Vars.content.getByName(ContentType.liquid, "adc-creotite");
 
 function newNode(parent, content, req, objectives){
   
@@ -13,6 +14,13 @@ function newNode(parent, content, req, objectives){
     node.objectives.addAll(objectives = null ? null : objectives);
 }
 
+//liquids
+TechTree.nodeProduce(Liquids.oil, creotite);
+//TechTree.nodeProduce(Liquids.cryofluid, ledonite);
+
+//items
+
+//blocks
 newNode(Blocks.parallax, turrets.impaler, ItemStack.with(Items.titanium, 7500, Items.silicon, 6900, Items.plastanium, 4450, Items.phaseFabric, 2000), Seq.with(new Objectives.SectorComplete(SectorPresets.impact0078)));
 newNode(Blocks.ripple, turrets.warhead, ItemStack.with(Items.lead, 8600, Items.graphite, 6400, Items.silicon, 2300, creostone, 1800, Items.plastanium, 1300), Seq.with(new Objectives.SectorComplete(SectorPresets.extractionOutpost), new Objectives.Research(Blocks.spectre)));
 newNode(Blocks.meltdown, turrets.needle, ItemStack.with(creostone, 10000, Items.silicon, 7000, Items.plastanium, 5400, Items.titanium, 4800), Seq.with(new Objectives.SectorComplete(SectorPresets.desolateRift), new Objectives.Research(Blocks.overdriveDome)));
