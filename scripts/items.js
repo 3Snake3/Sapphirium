80a54ef16d35b814d2467d9a30a0028e604f2d73
3Snake3 <sp1987it@gmail.com>
2021-10-03 17:27:43 +0800

    Small improvements

--- scripts/items.js
+++ scripts/items.js
@@ -1,4 +1,14 @@
 const surgeStone = extend(Item, "surge-stone", {});
+const chgStick = extend(Item, "charged-stick", {});
+const chgLead = extend(Item, "charged-lead", {});
+const chgIngot = extend(Item, "charged-ingot", {});
+const chgCyl = extend(Item, "charged-cylinder", {});
+const iceCube = extend(Item, "ice-cube", {});
+const creostone = extend(Item, "creostone", {});
+const cryoCube = extend(Item, "cryocube", {});
+const cinderblock = extend(Item, "cinderblock", {});
+const globium = extend(Item, "globium", {});
+const creosand = extend(Item, "creosand", {});
 
 module.exports = {
 surgeStone: SS
