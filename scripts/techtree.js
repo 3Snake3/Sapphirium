4b20d0c164a1369cc4fbb6568e059f2f6c572284
3Snake3 <sp1987it@gmail.com>
2021-10-18 13:40:54 +0800

    Bug fixes

--- scripts/techtree.js
+++ scripts/techtree.js
@@ -14,7 +14,7 @@ function newNode(parent, content, req, objectives){
 
 //blocks
 newNode(Blocks.parallax, turrets.impaler, ItemStack.with(Items.titanium, 7500, Items.silicon, 6900, Items.plastanium, 4450, Items.phaseFabric, 2000), Seq.with(new Objectives.SectorComplete(SectorPresets.impact0078)));
-newNode(Blocks.ripple, turrets.warhead, ItemStack.with(Items.lead, 8600, Items.graphite, 6400, Items.silicon, 2300, items.creostone, 1800, Items.plastanium, 1300), Seq.with(new Objectives.SectorComplete(SectorPresets.extractionOutpost), new Objectives.Research(Blocks.spectre)));
-newNode(Blocks.meltdown, turrets.needle, ItemStack.with(items.creostone, 10000, Items.silicon, 7000, Items.plastanium, 5400, Items.titanium, 4800), Seq.with(new Objectives.SectorComplete(SectorPresets.desolateRift), new Objectives.Research(Blocks.overdriveDome)));
+newNode(Blocks.ripple, turrets.warhead, ItemStack.with(Items.lead, 8600, Items.graphite, 6400, Items.silicon, 2300, Items.thorium, 1800, Items.plastanium, 1300), Seq.with(new Objectives.SectorComplete(SectorPresets.extractionOutpost), new Objectives.Research(Blocks.spectre)));
+newNode(Blocks.meltdown, turrets.needle, ItemStack.with(Items.graphite, 10000, Items.silicon, 7000, Items.plastanium, 5400, Items.titanium, 4800), Seq.with(new Objectives.SectorComplete(SectorPresets.desolateRift), new Objectives.Research(Blocks.overdriveDome)));
 newNode(Blocks.cryofluidMixer, blocks.multi, ItemStack.with(Items.lead, 1800, Items.graphite, 1000, Items.silicon, 1200), Seq.with(new Objectives.SectorComplete(SectorPresets.craters)));
 newNode(Blocks.thermalPump, blocks.upgPump, ItemStack.with(Items.graphite, 8500, Items.metaglass, 8000, Items.silicon, 7150, Items.titanium, 5000, Items.thorium, 4450), Seq.with(new Objectives.SectorComplete(SectorPresets.windsweptIslands)));
