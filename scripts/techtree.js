5fd54fbb977b9786fe4e00f84baa403ec023344d
3Snake3 <sp1987it@gmail.com>
2021-10-18 13:17:34 +0800

    Big fixes

--- scripts/techtree.js
+++ scripts/techtree.js
@@ -3,15 +3,16 @@ const turrets = require("blocks/turrets");
 const items = require("items");
 
 function newNode(parent, content, req, objectives){
-    var parnode = TechTree.get(parent);
-    var node = new TechTree.TechNode(parnode, content, req = null ? content.researchRequirements() : req);
-    var used = new ObjectSet();
+	
+var parnode = TechTree.get(parent);
+var node = new TechTree.TechNode(parnode, content, req = null ? content.researchRequirements() : req);
+var used = new ObjectSet();
   
     node.objectives.addAll(objectives = null ? null : objectives);
 }
   
 //blocks
-newNode(turrets.north, turrets.impaler, ItemStack.with(Items.titanium, 7500, Items.silicon, 6900, Items.plastanium, 4450, Items.phaseFabric, 2000), Seq.with(new Objectives.SectorComplete(SectorPresets.impact0078)));
+newNode(Blocks.parallax, turrets.impaler, ItemStack.with(Items.titanium, 7500, Items.silicon, 6900, Items.plastanium, 4450, Items.phaseFabric, 2000), Seq.with(new Objectives.SectorComplete(SectorPresets.impact0078)));
 newNode(Blocks.ripple, turrets.warhead, ItemStack.with(Items.lead, 8600, Items.graphite, 6400, Items.silicon, 2300, items.creostone, 1800, Items.plastanium, 1300), Seq.with(new Objectives.SectorComplete(SectorPresets.extractionOutpost), new Objectives.Research(Blocks.spectre)));
 newNode(Blocks.meltdown, turrets.needle, ItemStack.with(items.creostone, 10000, Items.silicon, 7000, Items.plastanium, 5400, Items.titanium, 4800), Seq.with(new Objectives.SectorComplete(SectorPresets.desolateRift), new Objectives.Research(Blocks.overdriveDome)));
 newNode(Blocks.cryofluidMixer, blocks.multi, ItemStack.with(Items.lead, 1800, Items.graphite, 1000, Items.silicon, 1200), Seq.with(new Objectives.SectorComplete(SectorPresets.craters)));
