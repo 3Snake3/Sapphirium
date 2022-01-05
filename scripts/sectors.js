461b2cc7fcdd9765735a5cd4b1035c6d227e2df7
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2022-01-05 16:37:43 +0300

    Update sectors.js

--- scripts/sectors.js
+++ scripts/sectors.js
@@ -1,4 +1,11 @@
-const expCreotite = extendContent(SectorPreset, "experiment-creotite", Planets.serpulo, 194, {
-captureWave: 50,
-difficulty: 6
+const creotitePowerStation = extendContent(SectorPreset, "creotite-power-station", Planets.serpulo, 194, {
+  localizedName: "Creotite Power Station",
+  captureWave: 50,
+  difficulty: 9,
+  startWaveTimeMultiplier: 1,
+  
+  loadIcon() {
+    this.super$loadIcon();
+    //may be custom icon
+  }
 });
