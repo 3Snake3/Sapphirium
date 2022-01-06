4b856846fda65625e21b48f85ccc33832ca88cf8
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2022-01-06 17:32:52 +0300

    Update sectors.js

--- scripts/sectors.js
+++ scripts/sectors.js
@@ -1,8 +1,8 @@
-const creotitePowerStation = new SectorPreset( "creotite-power-station", Planets.serpulo, 9);
+const creotitePowerStation = new SectorPreset( "creotite-power-station", Planets.serpulo, 140);
   
+  creotitePowerStation.captureWave = 20;
   creotitePowerStation.localizedName = "Creotite Power Station";
   creotitePowerStation.description = "A power plant destroyed by an explosion of an overheated creotite reactor. [red]Don't repeat the mistakes of the past!";
-  creotitePowerStation.captureWave = 20;
   creotitePowerStation.difficulty = 10;
   //for debug
   creotitePowerStation.alwaysUnlocked = false;
