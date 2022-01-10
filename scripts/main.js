8f4448cc3452531a85733ef9b42b3c2f74ce668c
3Snake3 <sp1987it@gmail.com>
2022-01-11 02:49:17 +0800

    Mod rename

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
   
 const mod = Vars.mods.locateMod("adc");
-mod.meta.displayName = "[#8ca4f5]ADC";
+mod.meta.displayName = "[#8ca4f5]Sapphirium";
 mod.meta.description = "Adds a lot of additional content like turrets, factories and other.";
 mod.meta.author = "[green]Snake[white] & [red]Super[green]Jaba[red]2000";
 
