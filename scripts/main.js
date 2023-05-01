a83aa54b6c31d6ef0e3e30c3be4130a43823e6a4
3Snake3 <sp1987it@gmail.com>
2023-05-01 20:47:52 +0800

    Some Fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
   
 const mod = Vars.mods.locateMod("sapphirium");
 mod.meta.displayName = "[#8ca4f5]Sapphirium";
-mod.meta.description = "Adds a lot of additional content like turrets, factories and other.";
+mod.meta.description = "Ah the Sapphirium, sounds good right? \nLet us introduce the list of content it includes: \nA hell amount of fraction turrets, brand new fraction units and in the end new interesting materials. \nIn next updates we want to introduce some pretty big expansion to erekir, stay tune. \n\nContentmaker & Helpers List: \n[accent]Kostolom[] - Balance maker, map maker, source of ideas. Discord profile kostolom#9777 \n[#00FF00]Laim[] - Spriter, map maker, a small helper with a code. Discord profile LaIm-Ik.json#5237 \n[green]Liplum[] - Java coder. Discord profile Liplum#2623 \n[#b88cff]Ferret Fantasy[] - Spriter. Discord profile sundered#8134 \n[#ecf2aa]Dr.Shramberg[] - A small script helper. Discord profile ADG021222#7589 \n[red]Fox1va[] - A small helper. Discord profile Fox1va_#3532 \n[#c1befa]Vervalamis[] - Spriter. Discord profile Saigo no-nozomi#1206 \n\nThanks everyone for support.";
 mod.meta.author = "[green]Snake";
 
 require("blocks/walls");
