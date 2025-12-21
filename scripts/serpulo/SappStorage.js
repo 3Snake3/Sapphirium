893fd405c39e215ab6d415e32c8a5a30ae471096
3Snake3 <sp1987it@gmail.com>
2025-12-21 17:44:24 +0800

    Fix #3

--- scripts/serpulo/SappStorage.js
+++ scripts/serpulo/SappStorage.js
@@ -67,7 +67,7 @@ var cone = 360;
 //the inaccuracy of bullets
 var healBulletInaccuracy = 6;
 //shoot sound
-var healBulletSound = Sounds.missile;
+var healBulletSound = Sounds.shotMissile;
 
 
 var emerald = items.emerald;
