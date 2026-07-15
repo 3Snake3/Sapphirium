dcaba6c0d77ba6e446309bc52ff53fdc4fa29836
3Snake3 <sp1987at@gmail.com>
2026-07-15 22:58:24 +0800

    Fixed crystal vein

--- scripts/erekir/SappErekirProduction.js
+++ scripts/erekir/SappErekirProduction.js
@@ -1,5 +1,6 @@
 const topazCrucible = extend(HeatCrafter, "topaz-crucible", {});
 const sapphireSynthesizer = extend(HeatCrafter, "sapphire-synthesizer", {});
+const crystalVein = extend(HeatCrafter, "crystal-vein", {});
 const rubyConverter = extend(HeatCrafter, "ruby-converter", {});
 const neoplasmConverter = extend(GenericCrafter, "neoplasm-converter", {});
 const creostoneCrucible = extend(GenericCrafter, "creostone-crucible", {});
